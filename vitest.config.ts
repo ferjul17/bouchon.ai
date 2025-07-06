2656129e35d4885432473dd35cd4618371828800
julien <julien@ferrier.email>
2025-07-06 12:47:08 +0200

    fix: Exclude index.ts from coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
       include: ['src/**/*.ts'],
-      exclude: ['src/types/**/*'],
+      exclude: ['src/types/**/*', 'src/index.ts'],
       thresholds: {
         lines: 95,
         functions: 95,
