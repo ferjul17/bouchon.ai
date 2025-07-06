40986baa6e92390993c6f3e73b56e384af23fd0c
julien <julien@ferrier.email>
2025-07-06 12:46:23 +0200

    fix: Disable problematic ESLint rules

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -5,5 +5,12 @@ export default antfu({
   typescript: true,
   rules: {
     'no-console': 'off',
+    'style/comma-dangle': 'off',
+    'style/brace-style': 'off',
+    'style/arrow-parens': 'off',
+    'style/jsx-one-expression-per-line': 'off',
+    'style/member-delimiter-style': 'off',
+    'antfu/if-newline': 'off',
+    'style/multiline-ternary': 'off',
   },
 })
